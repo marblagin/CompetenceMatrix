ccb3479edae0d4d2ce2a35a74e4bd36989bdd9d0
Llewellyn Craddock <lacraddock@gmail.com>
2015-05-26 21:29:52 +0200

    Initialized Git and GUI

---
+++ src/Classes/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import GUI.*;
+
+/**
+ *
+ * @author Llewellyn Craddock
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mf = new MainFrame ();
+        mf.setVisible(true);
+        SearchFrame sf = new SearchFrame ();
+        sf.setVisible(false);
+    }
+
+}
